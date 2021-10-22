83b34058b1d1f432a45a752e832ce0087cd29de3
Corbin Ball <c.ball@me.com>
2021-10-22 18:13:02 -0500

    fixed typo/bug

--- Develop/routes/htmlroutes.js
+++ Develop/routes/htmlroutes.js
@@ -8,12 +8,12 @@ const {
 
 // add get routes for notes.html and index.html
 
-html.get('/notes', (req, res) =>
+html.get('/notes', (req, res) => {
 res.sendFile(path.join(__dirname, '../public/notes.html'))
-);
+});
 
-html.get('*', (req, res) =>
+html.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'))
-);
+});
 
 module.exports = html;
