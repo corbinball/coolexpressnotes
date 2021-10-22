b96f7e8f3766a775821f9e59278d0a3593714f6a
Corbin Ball <c.ball@me.com>
2021-10-22 15:31:31 -0500

    added route to post a note

--- Develop/routes/note.js
+++ Develop/routes/note.js
@@ -9,6 +9,32 @@ const {
 
 //get route to return all saveds notes from the db.json file
 
-notes.get('/', (req, res) => {
+notes.get('/api/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
+
+
+
+  //post route to create a new note
+
+  notes.post('/', (req, res) => {
+    console.log(req.body);
+  
+    const { title, text } = req.body;
+  
+    if (req.body) {
+      const newTip = {
+        title,
+        text,
+        tip_id: uuid(),
+      };
+  
+      readAndAppend(newTip, './db/db.json');
+      res.json(`YAY! Note added.`);
+    } else {
+      res.error('Error! Something went wrong');
+    }
+  });
+
+
+  
