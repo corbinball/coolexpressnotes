47fb70986cb1b2fe06d9a44881b7051b1b67476b
Corbin Ball <c.ball@me.com>
2021-10-22 18:25:28 -0500

    unsure of changes

--- Develop/routes/htmlroutes.js
+++ Develop/routes/htmlroutes.js
@@ -1,19 +1,14 @@
 const html = require('express').Router();
 const path = require('path');
-const uuid = require('uuid');
-const {
-    readFromFile,
-    readAndAppend,
-  } = require('../helpers/fsUtils');
 
 // add get routes for notes.html and index.html
 
 html.get('/notes', (req, res) => {
-res.sendFile(path.join(__dirname, '../public/notes.html'))
+res.sendFile(path.join(__dirname, './public/notes.html'))
 });
 
 html.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
 module.exports = html;
